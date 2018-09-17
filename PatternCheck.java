1b80e278ea2e5b2b3129e3feb80afd64f3c4383a
Saikrishna <saikrishnakeerti@gmail.com>
2018-09-17 13:41:06 +0530

    Update PatternCheck.java

--- PatternCheck.java
+++ PatternCheck.java
@@ -5,13 +5,15 @@ public class PatternCheck {
     public static void main(String[] args) {
         String str = "This is simple example to check wheather a sentence starts " +
                 "with capital letter and ends with a period.";
+        //taking a sample string as input
         Pattern p = Pattern.compile("^[A-Z].*\\.$");
         Matcher m = p.matcher(str);
-                // creating a pattern to check whether the string matches the required condition
+        // creating a pattern to check whether the string matches the required condition
 
-        if(m.find())
+        if (m.find())
             System.out.println("given string matches the pattern");
         else
             System.out.println("given string does not match the pattern");
+        // printing the result
     }
 }
